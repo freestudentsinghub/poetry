a530cf09d57784567a4309e77eba368e68b9d2a8
freestudentsinghub <masha1207355@gmail.com>
2024-09-18 17:34:07 +0300

    complite

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.work import Product, Category
+from src.work import Category, Product
 
 
 @pytest.fixture
@@ -9,20 +9,22 @@ def product():
         name="Samsung Galaxy S23 Ultra",
         description="Samsung Galaxy S23 Ultra",
         price=180000,
-        quantity=5
+        quantity=5,
     )
 
 
 @pytest.fixture
 def category():
-    product1 = Product("Samsung Galaxy S23 Ultra", "Samsung Galaxy S23 Ultra", 180000.0, 5)
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "Samsung Galaxy S23 Ultra", 180000.0, 5
+    )
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
     return Category(
         name="Смартфоны",
-        description="""Смартфоны, как средство не только коммуникации, 
-        но и получения дополнительных функций для удобства жизни""",
-        products=[product1, product2, product3]
+        description="Смартфоны, как средство не только коммуникации, "
+                    "но и получения дополнительных функций для удобства жизни",
+        products=[product1, product2, product3],
     )
 
 
@@ -35,8 +37,11 @@ def test_product_init(product):
 
 def test_category_init(category):
     assert category.name == "Смартфоны"
-    assert category.description == """Смартфоны, как средство не только коммуникации, 
-        но и получения дополнительных функций для удобства жизни"""
+    assert (
+        category.description
+        == "Смартфоны, как средство не только коммуникации, "
+           "но и получения дополнительных функций для удобства жизни"
+    )
     assert len(category.products) == 3
 
     assert category.category_count == 1
