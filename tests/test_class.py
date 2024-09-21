2afe978a44be182345b0e054145c89874c7850bc
freestudentsinghub <masha1207355@gmail.com>
2024-09-21 13:16:07 +0300

    add homework 14.2

--- tests/test_class.py
+++ tests/test_class.py
@@ -23,7 +23,7 @@ def category():
     return Category(
         name="Смартфоны",
         description="Смартфоны, как средство не только коммуникации, "
-                    "но и получения дополнительных функций для удобства жизни",
+        "но и получения дополнительных функций для удобства жизни",
         products=[product1, product2, product3],
     )
 
@@ -38,11 +38,41 @@ def test_product_init(product):
 def test_category_init(category):
     assert category.name == "Смартфоны"
     assert (
-        category.description
-        == "Смартфоны, как средство не только коммуникации, "
-           "но и получения дополнительных функций для удобства жизни"
+        category.description == "Смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни"
     )
-    assert len(category.products) == 3
+    assert len(category.products) == 146
 
     assert category.category_count == 1
     assert category.product_count == 3
+
+
+def test_products_property(category):
+    assert category.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+    )
+
+
+def test_add_product(category):
+    assert category.product_count == 9
+
+
+def test_new_product():
+    products = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+    products.name = "Samsung Galaxy S23 Ultra"
+    products.description = "256GB, Серый цвет, 200MP камера"
+    products.price = 180000.0
+    products.quantity = 5
+
+
+def test_price_setter(capsys, product):
+    product.price = -100
+    messange = capsys.readouterr()
+    assert messange.out.strip() == "Цена не должна быть нулевая или отрицательная"
+
+    product.price = 800
+    assert product.price == 800
