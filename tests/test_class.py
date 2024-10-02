490807c122f3c06d0b9f6d639e0cef9c82bfe9c2
freestudentsinghub <masha1207355@gmail.com>
2024-10-02 21:31:25 +0300

    add update nwe homework 16.2

--- tests/test_class.py
+++ tests/test_class.py
@@ -196,3 +196,33 @@ def test_lawngrass_init(lawngrass):
     assert lawngrass.country == "Россия"
     assert lawngrass.germination_period == "7 дней"
     assert lawngrass.color == "Зеленый"
+
+
+def test_mixin_print(capsys):
+    Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    message = capsys.readouterr()
+    assert message.out.strip() == 'Product(55" QLED 4K, Фоновая подсветка, 123000.0, 7)'
+
+    Smartphone(
+        "Iphone 15", "512GB, Gray space", 210000.0, 8,
+        98.2, "15", 512, "Gray space"
+    )
+    message = capsys.readouterr()
+    assert (
+        message.out.strip() == "Smartphone(Iphone 15, 512GB, Gray space, 210000.0, 8)"
+    )
+
+    LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый",
+    )
+    message = capsys.readouterr()
+    assert (
+        message.out.strip()
+        == "LawnGrass(Газонная трава, Элитная трава для газона, 500.0, 20)"
+    )
